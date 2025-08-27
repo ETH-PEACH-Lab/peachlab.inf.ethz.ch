c0784e230cc6bf3a926653affc709058cea04a65
April Wang from ETHZ <april.wang@inf.ethz.ch>
2025-08-27 20:08:53 +0200

    ignore error with raw loader

--- app/projects/[slug]/blogImports.jsx
+++ app/projects/[slug]/blogImports.jsx
@@ -1,5 +1,6 @@
 // app/projects/[slug]/blogImports.js
 // Use raw-loader (or MDX) but always wrap in a function that returns a module-like object
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import spark from '!raw-loader!@/data/blog/spark.md';
 // import teaching from '!raw-loader!@/data/blog/teaching.md';
 
