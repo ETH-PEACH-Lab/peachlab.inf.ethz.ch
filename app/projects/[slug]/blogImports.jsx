cd0cf9ad7480256a7809b1c29eb5633bd18fd987
April Wang from ETHZ <april.wang@inf.ethz.ch>
2025-10-15 21:09:14 +0200

    fix the raw loader syntax issue

--- app/projects/[slug]/blogImports.jsx
+++ app/projects/[slug]/blogImports.jsx
@@ -1,9 +1,11 @@
 // app/projects/[slug]/blogImports.js
 // Use raw-loader (or MDX) but always wrap in a function that returns a module-like object
+
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import spark from '!raw-loader!@/data/blog/spark.md';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import emotion from '!raw-loader!@/data/blog/emotion.md';
-// import teaching from '!raw-loader!@/data/blog/teaching.md';
 
 export const blogImports = {
   spark: async () => ({ default: spark }),
