cf977eac4ebd90ba239f8cb57975b0c2a1edf1a2
Yang Wu <yangwu@Yangs-MacBook-Pro.local>
2026-01-26 20:55:16 +0100

    add stepmind blog

--- app/projects/[slug]/blogImports.jsx
+++ app/projects/[slug]/blogImports.jsx
@@ -7,9 +7,13 @@ import spark from '!raw-loader!@/data/blog/spark.md';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import emotion from '!raw-loader!@/data/blog/emotion.md';
 
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import stepmind from '!raw-loader!@/data/blog/stepmind.md';
+
 export const blogImports = {
   spark: async () => ({ default: spark }),
   emotion: async () => ({ default: emotion }),
+   stepmind: async () => ({ default: stepmind }),
   // teaching: async () => ({ default: teaching }),
   // add more slugs...
 };
