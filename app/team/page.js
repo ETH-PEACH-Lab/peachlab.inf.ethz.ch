384fcd0de9ff6920040623630766b1142725991b
April Wang from ETHZ <april.wang@inf.ethz.ch>
2026-03-31 17:03:55 +0200

    update funding

--- app/team/page.js
+++ app/team/page.js
@@ -14,6 +14,7 @@ export default function Team() {
         <div>
             <h2>Team</h2>
 
+            <p>For general inquiries about the lab, please contact us at <a href="mailto:peachlab@inf.ethz.ch">peachlab@inf.ethz.ch</a>.</p>
             <h3>Core Members</h3>
             <Grid.Container gap={2} style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea" }}>
                 {currentMembers.map((member) => (
@@ -21,36 +22,86 @@ export default function Team() {
                 ))}
             </Grid.Container>
 
-            <h3>Affiliated Members</h3>
-            <ul style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea" }}>
-                {affiliatedMembers.map((member) => (
-                    <li key={member.name}>
-                        {member.link ? (
-                            <a href={member.link} target="_blank" rel="noopener noreferrer">
-                                {member.name}
-                            </a>
-                        ) : (
-                            member.name
-                        )}
-                        {", "}
-                        <span dangerouslySetInnerHTML={{ __html: member.title }} />
-                    </li>
-                ))}
-            </ul>
+            <section style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea", paddingBottom: "20px" }}>
 
-            <h3>Join Us</h3>
-            <ul>
-                <li>
-                    We are always excited to work with motivated and talented students. Please check
-                    {" "}
-                    <Link href="/join">join us page</Link> for more details.
-                </li>
-                <li>
-                    If you are an ETH Zurich student looking for a BSc or MSc thesis, please check
-                    {" "}
-                    <Link href="/theses">theses page</Link> for more details.
-                </li>
-            </ul>
+                <h3>Affiliated Members</h3>
+                <ul style={{ marginBottom: "20px" }}>
+                    {affiliatedMembers.map((member) => (
+                        <li key={member.name}>
+                            {member.link ? (
+                                <a href={member.link} target="_blank" rel="noopener noreferrer">
+                                    {member.name}
+                                </a>
+                            ) : (
+                                member.name
+                            )}
+                            {", "}
+                            <span dangerouslySetInnerHTML={{ __html: member.title }} />
+                        </li>
+                    ))}
+                </ul>
+            </section>
+
+            <section style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea", paddingBottom: "20px" }}>
+                <h3>Join Us</h3>
+                <ul>
+                    <li>
+                        We are always excited to work with motivated and talented students. Please check
+                        {" "}
+                        <Link href="/join">join us page</Link> for more details.
+                    </li>
+                    <li>
+                        If you are an ETH Zurich student looking for a BSc or MSc thesis, please check
+                        {" "}
+                        <Link href="/theses">theses page</Link> for more details.
+                    </li>
+                </ul>
+            </section>
+
+
+            <section style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea", paddingBottom: "20px" }}>
+
+                <h3>Funding & Support</h3>
+
+                <p>Our lab’s research is made possible through the generous support of public funding agencies, foundations, and industry partners. Interested in supporting our work? Get in touch.</p>
+                <div style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    alignItems: "center",
+                    gap: "2rem",
+                    marginBottom: "20px"
+                }}>
+                    <a href="https://research.adobe.com/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/Adobe.png" alt="Adobe Research" style={{ height: "50px" }} />
+                    </a>
+                    <a href="https://www.snf.ch/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/snsf.png" alt="Swiss National Science Foundation" style={{ height: "50px" }} />
+                    </a>
+                    <a href="https://www.dieter-schwarz-stiftung.de/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/dieter-schwarz.png" alt="Dieter Schwarz Foundation" style={{ height: "50px" }} />
+                    </a>
+                    <a href="https://ethz-foundation.ch/en/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/eth-foundation.png" alt="ETH Foundation" style={{ height: "50px" }} />
+                    </a>
+                    <a href="https://www.swiss-ai.org/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/swiss-ai.jpg" alt="Swiss AI" style={{ height: "50px" }} />
+                    </a>
+                    <a href="https://ai.ethz.ch/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/sponsors/ai-center.png" alt="ETH AI Center" style={{ height: "50px" }} />
+                    </a>
+                </div>
+            </section>
+
+            <section style={{ marginBottom: "20px", borderBottom: "1px solid #eaeaea", paddingBottom: "20px" }}>
+                <h3>Outreach</h3>
+                <p>
+                    We collaborate with local schools and coding camps to both study and actively support STEM outreach, testing and refining our educational interventions in real-world settings, including
+                    <a href="https://www.edelweiss-school.ch/" target="_blank" rel="noopener noreferrer">Edelweiss School</a>,
+                    <a href="https://ingch.ch/en/" target="_blank" rel="noopener noreferrer">IngCH</a>,
+                    and our
+                    <a href="https://inf.ethz.ch/communication-services/schools-universities.html" target="_blank" rel="noopener noreferrer">department's communication team</a>.
+                    We welcome educators, schools, and organizations interested in STEM outreach to reach out to us for potential collaboration and support.
+                </p>            </section>
 
             <div style={{ marginTop: "4rem" }} />
 
