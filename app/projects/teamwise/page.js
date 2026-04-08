063c62c288792687a1d6d2b5eb4f2967d375ac1a
April Wang from ETHZ <april.wang@inf.ethz.ch>
2026-04-08 16:00:35 +0200

    setup teamwise page

---
+++ app/projects/teamwise/page.js
@@ -0,0 +1,11 @@
+'use client';
+import "./style.css";
+
+export default function TeamwisePage() {
+    return (
+        <div>
+            <h2>Teamwise</h2>
+            <p>This is where you can make changes</p>
+        </div>
+    );
+}
